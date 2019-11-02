474545fcaf171be88c9da28c7d2d394e6cbe9542
Jhonatt Lima Dos Santos <19191763@pucrs.br>
2019-11-01 22:00:18 -0300

    Added movement and jump

--- Assets/Scripts/Kirby_Movement.cs
+++ Assets/Scripts/Kirby_Movement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Kirby_Movement : MonoBehaviour
+public class Kirby_movement : MonoBehaviour
 {
     public float moveSpeed = 1f;
     public float turnSpeed = 200f;
@@ -12,8 +12,9 @@ public class Kirby_Movement : MonoBehaviour
     private float _verticalSpeed;
     private float _jumpSpeed;
     private float _maxJumpCooldown;
-    private float _horizontalInput;
-    private float _verticalInput;
+    private float _inputHorizontal;
+    private float _inputVertical;
+    private Vector3 movement;
 
     void Awake()
     {
@@ -21,53 +22,42 @@ public class Kirby_Movement : MonoBehaviour
         _jumpSpeed = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * jumpHeight);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        jump();
+        move();
+        applyGravity();
     }
 
-    private void jumpCheck()
+    private void jump()
     {
-        if(_characterController.isGrounded && Input.GetKeyDown(KirbyConstants.keyJump))
+        if(Input.GetKeyDown(KirbyConstants.keyJump))
         {
-            _verticalSpeed = _jumpSpeed;   
-
+            if(_characterController.isGrounded)
+            {
+                _verticalSpeed = _jumpSpeed;
+            }
+            else
+            {
+                _verticalSpeed = _jumpSpeed / 2;
+            }  
         }
+    }
 
-        // if (_controller.isGrounded)
-        // {
-        //     _verticalSpeed = 0f;
+    private void move()
+    {
+        _inputHorizontal = Input.GetAxis("Horizontal");
+        movement = transform.right * _inputHorizontal * moveSpeed;
+        movement.y = _verticalSpeed;
+        movement = movement * Time.deltaTime;
+        _characterController.Move(movement);
+    }
 
-        //     if (Input.GetKeyDown(KeyCode.Space))
-        //     {
-        //         _verticalSpeed = _jumpSpeed;
-        //         _jumpStartHeight = transform.position.y;
-        //     }
-
-        //     _maxJumpHeight = 0f;
-        // }
-        // else
-        // {
-        //     // aplicar gravidade
-        //     _verticalSpeed += Physics.gravity.y * Time.deltaTime;
-
-        //     float jumpHeight = transform.position.y - _jumpStartHeight;
-        //     _maxJumpHeight = Mathf.Max(_maxJumpHeight, jumpHeight);
-        //     Debug.LogFormat("i'M JUMPING YO jumpHeight:{0} _maxJumpHeight:{1}",
-        //         jumpHeight, _maxJumpHeight);
-        // }
-
-        // Vector3 moveSpeed = transform.forward * verticalInput * MoveSpeed;
-        // moveSpeed.y = _verticalSpeed;
-        // Vector3 frameMovement = moveSpeed * Time.deltaTime;
-
-        // _controller.Move(frameMovement);
-
-
-
-        // //_controller.SimpleMove(moveSpeed);
-
-        // //_controller.ju
+    private void applyGravity()
+    {
+        if(!_characterController.isGrounded)
+        {
+            _verticalSpeed += Physics.gravity.y * Time.deltaTime;
+        }
     }
 }
