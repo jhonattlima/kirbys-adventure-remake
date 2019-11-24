7c5c58308a2196f43966aa0a94dbb81d99962b9d
Jhonatt <jhonattlima@gmail.com>
2019-11-24 12:57:38 -0300

    Addes UIs and Stage

---
+++ Assets/Scripts/System/SystemConstants.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemConstants 
+{
+    public const string NETWORK_NAME_LOCAL_CLIENT = "localClient";
+    public const string NETWORK_NAME_LOCAL_SERVER = "localServer";
+
+    public const string BUTTON_NAME_LAN_MODE = "ButtonLanMode";
+    public const string BUTTON_NAME_ONLINE_MODE = "ButtonOnlineMode";
+    public const string BUTTON_NAME_QUIT_MODE = "ButtonQuit";
+
+    public const string BUTTON_NAME_CREATE_GAME = "ButtonCreateGame";
+    public const string BUTTON_NAME_JOIN_GAME = "ButtonJoinGame";
+    public const string BUTTON_NAME_BACK = "ButtonBack";
+}
