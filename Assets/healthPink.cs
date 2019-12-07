4b34ed23d933e4b66784e9522819b818ee167aeb
vicenteraiol <55415822+vicenteraiol@users.noreply.github.com>
2019-12-07 01:55:55 -0300

    First UI updates
    Pink UI HP and power icons added

---
+++ Assets/healthPink.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthPink : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    public Image[] bars;
+    public Sprite fullBar;
+    public Sprite emptyBar;
+
+    void Update()
+    {
+        if (currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        }
+
+        for (int i = 0; i < bars.Length; i++) {
+
+            //Current health control
+            if (i < currentHealth)
+            {
+                bars[i].sprite = fullBar;
+            } else {
+                bars[i].sprite = emptyBar;
+            }
+
+            // Max health control
+            if (i < maxHealth) {
+                bars[i].enabled = true;
+            } else {
+                bars[i].enabled = false;
+            }
+        }
+    }
+}
