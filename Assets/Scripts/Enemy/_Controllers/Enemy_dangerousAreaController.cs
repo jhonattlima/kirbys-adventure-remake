58e0b06a00c647943fd091b6a6e07419f9a55380
jhonattlima <jhonattlima@gmail.com>
2019-12-08 12:24:21 -0300

    Created enemy dangerous area for powers

---
+++ Assets/Scripts/Enemy/_Controllers/Enemy_dangerousAreaController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_dangerousAreaController : MonoBehaviour
+{
+    int powerDamage = 1;
+
+    void OnTriggerEnter(Collider other)
+    {
+        execKirbyDamage(other);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        execKirbyDamage(other);
+    }
+
+    void execKirbyDamage(Collider other)
+    {
+        if(other.CompareTag(KirbyConstants.TAG_PLAYER))
+        {
+            if(!other.GetComponent<Kirby_actor>().isLocalPlayer) return;
+            other.gameObject.GetComponent<Kirby_healthController>().takeDamage(powerDamage);
+        }
+    }
+}
--- Assets/Scripts/Enemy/_Controllers/Enemy_playerCloseController.cs
+++ Assets/Scripts/Enemy/_Controllers/Enemy_playerCloseController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Enemy_playerCloseController : MonoBehaviour
 {
-    public int numberOfPlayersClose;
+    public int numberOfPlayersClose = 0;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag(KirbyConstants.TAG_PLAYER))
         {
-            // Debug.Log("Player entered on enemyArea.");
+            Debug.Log("Player entered on enemyArea.");
             numberOfPlayersClose ++;
         }
     }
@@ -19,9 +19,9 @@ public class Enemy_playerCloseController : MonoBehaviour
     {
         if(other.CompareTag(KirbyConstants.TAG_PLAYER))
         {
-            // Debug.Log("Player left on enemyArea.");
+            Debug.Log("Player left on enemyArea.");
             numberOfPlayersClose --;
-            if(numberOfPlayersClose == 0) GameManager.instance.localPlayer.kirbyServerController.CmdDestroyPrefab(transform.parent.gameObject);
+            if(numberOfPlayersClose <= 0) GameManager.instance.localPlayer.kirbyServerController.CmdDestroyPrefab(transform.parent.gameObject);
         }
     }
 }
